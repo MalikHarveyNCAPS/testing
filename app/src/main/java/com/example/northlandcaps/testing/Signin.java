5ac1ac5ad3bcaa8fd92de53f79f2e49a859592bf
Malik Harvey <malik.harvey@northlandcaps.org>
2018-12-07 09:53:22 -0600

    notif works for older phones

--- app/src/main/java/com/example/northlandcaps/testing/Signin.java
+++ app/src/main/java/com/example/northlandcaps/testing/Signin.java
@@ -17,31 +17,33 @@ import com.google.firebase.auth.FirebaseUser;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Signin extends AppCompatActivity {
-    Button signout,notifcation;
+    Button signout, notifcation;
     private FirebaseAuth mAuth;
     TextView username;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
 
-        mAuth=FirebaseAuth.getInstance();
-        signout=findViewById(R.id.signout);
+        mAuth = FirebaseAuth.getInstance();
+        signout = findViewById(R.id.signout);
         username = findViewById(R.id.tvName);
-        notifcation=findViewById(R.id.notification);
+        notifcation = findViewById(R.id.notification);
 
-        if(mAuth.getCurrentUser()==null){
+        if (mAuth.getCurrentUser() == null) {
             finish();
-            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
         }
 
         //Fetch the Display name of current User
         FirebaseUser user = mAuth.getCurrentUser();
 
-        if(user!=null){
+        if (user != null) {
             username.setText("Welcome " + user.getDisplayName());
         }
 
@@ -51,7 +53,7 @@ public class Signin extends AppCompatActivity {
             public void onClick(View v) {
                 mAuth.signOut();
                 finish();
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
         });
 
@@ -64,76 +66,72 @@ public class Signin extends AppCompatActivity {
 
     }
 
-    private void sendNotification()
-    {
-
-        Toast.makeText(this, "Current Recipients is : user1@gmail.com ( Just For Demo )", Toast.LENGTH_SHORT).show();
-
+    private void sendNotification() {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                int SDK_INT = android.os.Build.VERSION.SDK_INT;
-                if (SDK_INT > 8) {
-                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                            .permitAll().build();
-                    StrictMode.setThreadPolicy(policy);
-                    String send_email;
-
-                    //This is a Simple Logic to Send Notification different Device Programmatically....
-                    if (MainActivity.LoggedIn_User_Email.equals("user1@gmail.com")) {
-                        send_email = "user2@gmail.com";
-                    } else {
-                        send_email = "user1@gmail.com";
+                final String[] SynergyEmails = {"harveym466@gmail.com", "ruinhard@gmail.com"};
+                for (int y = 0; y < SynergyEmails.length; y++) {
+                    if (SynergyEmails[y].equals(MainActivity.LoggedIn_User_Email)) {
+                        continue;
                     }
+                    int SDK_INT = android.os.Build.VERSION.SDK_INT;
+                    if (SDK_INT > 8) {
+                        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                                .permitAll().build();
+                        StrictMode.setThreadPolicy(policy);
 
-                    try {
-                        String jsonResponse;
+                        String send_email= SynergyEmails[y];
 
-                        URL url = new URL("https://onesignal.com/api/v1/notifications");
-                        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                        con.setUseCaches(false);
-                        con.setDoOutput(true);
-                        con.setDoInput(true);
+                        try {
+                            String jsonResponse;
 
-                        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-                        con.setRequestProperty("Authorization", "Basic YTUzMWE2M2UtMTZiYy00M2FhLWEyMjItYWQ5YWI1MDgzM2U2");
-                        con.setRequestMethod("POST");
+                            URL url = new URL("https://onesignal.com/api/v1/notifications");
+                            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                            con.setUseCaches(false);
+                            con.setDoOutput(true);
+                            con.setDoInput(true);
 
-                        String strJsonBody = "{"
-                                + "\"app_id\": \"0523d5af-d75a-4916-a8dd-3e9109e0f10b\","
+                            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                            con.setRequestProperty("Authorization", "Basic Mzk2YWEzOGUtMWEwNC00ZmZkLWFiMTQtN2JkYjNlNWZkYTk0");
+                            con.setRequestMethod("POST");
 
-                                + "\"filters\": [{\"field\": \"tag\", \"key\": \"User_ID\", \"relation\": \"=\", \"value\": \"" + send_email + "\"}],"
+                            String strJsonBody = "{"
+                                    + "\"app_id\": \"43140cf2-d7a7-4430-8404-e0c6e644a11e\","
 
-                                + "\"data\": {\"foo\": \"bar\"},"
-                                + "\"contents\": {\"en\": \"English Message\"}"
-                                + "}";
+                                    + "\"filters\": [{\"field\": \"tag\", \"key\": \"User_ID\", \"relation\": \"=\", \"value\": \"" + send_email + "\"}],"
+
+                                    + "\"data\": {\"foo\": \"bar\"},"
+                                    + "\"contents\": {\"en\": \"EMERGENCY\"}"
+                                    + "}";
 
 
-                        System.out.println("strJsonBody:\n" + strJsonBody);
+                            System.out.println("strJsonBody:\n" + strJsonBody);
 
-                        byte[] sendBytes = strJsonBody.getBytes("UTF-8");
-                        con.setFixedLengthStreamingMode(sendBytes.length);
+                            byte[] sendBytes = strJsonBody.getBytes("UTF-8");
+                            con.setFixedLengthStreamingMode(sendBytes.length);
 
-                        OutputStream outputStream = con.getOutputStream();
-                        outputStream.write(sendBytes);
+                            OutputStream outputStream = con.getOutputStream();
+                            outputStream.write(sendBytes);
 
-                        int httpResponse = con.getResponseCode();
-                        System.out.println("httpResponse: " + httpResponse);
+                            int httpResponse = con.getResponseCode();
+                            System.out.println("httpResponse: " + httpResponse);
 
-                        if (httpResponse >= HttpURLConnection.HTTP_OK
-                                && httpResponse < HttpURLConnection.HTTP_BAD_REQUEST) {
-                            Scanner scanner = new Scanner(con.getInputStream(), "UTF-8");
-                            jsonResponse = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
-                            scanner.close();
-                        } else {
-                            Scanner scanner = new Scanner(con.getErrorStream(), "UTF-8");
-                            jsonResponse = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
-                            scanner.close();
+                            if (httpResponse >= HttpURLConnection.HTTP_OK
+                                    && httpResponse < HttpURLConnection.HTTP_BAD_REQUEST) {
+                                Scanner scanner = new Scanner(con.getInputStream(), "UTF-8");
+                                jsonResponse = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
+                                scanner.close();
+                            } else {
+                                Scanner scanner = new Scanner(con.getErrorStream(), "UTF-8");
+                                jsonResponse = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
+                                scanner.close();
+                            }
+                            System.out.println("jsonResponse:\n" + jsonResponse);
+
+                        } catch (Throwable t) {
+                            t.printStackTrace();
                         }
-                        System.out.println("jsonResponse:\n" + jsonResponse);
-
-                    } catch (Throwable t) {
-                        t.printStackTrace();
                     }
                 }
             }
